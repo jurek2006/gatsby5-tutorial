10d381477b10f9431b13568e21db4edcb17603f5
Jurek <jurek2006@gmail.com>
2023-04-13 14:55:35 +0200

    Finished tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,5 @@ module.exports = {
     title: `Gatsby5 tutorial`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
