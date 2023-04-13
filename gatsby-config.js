9b901759c5546050b60e1fe9fefe265922dfd339
Jurek <jurek2006@gmail.com>
2023-04-13 11:14:07 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby5 tutorial`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
